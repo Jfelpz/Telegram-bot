d160bf90bcb34247e8f60c7c4614cfd82649ee00
Jfelpz <joaozimalencar@gmail.com>
2026-06-28 20:33:29 -0300

    config.py

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 # ==========================
 # AUTHENTICADOR
 # ==========================
-
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 SHEET_ID = os.getenv("SHEET_ID")
 GOOGLE_CREDENTIALS = os.getenv("GOOGLE_CREDENTIALS")
 
