0000804c51a5329c75f03a0a0ed625eeb9eb2081
Jfelpz <joaozimalencar@gmail.com>
2026-06-28 20:27:40 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,38 +1,15 @@
-import os
-import json
 import uuid
 import time
-import gspread
 import requests
 from datetime import datetime
-from oauth2client.service_account import ServiceAccountCredentials
 from zoneinfo import ZoneInfo
 
 # ==========================
 # AUTH
 # ==========================
 
-SHEET_ID = os.getenv("SHEET_ID")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
-GOOGLE_CREDENTIALS = os.getenv("GOOGLE_CREDENTIALS")
-
-scope = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/drive"
-]
-
-client = gspread.authorize(
-    ServiceAccountCredentials.from_json_keyfile_dict(
-        json.loads(GOOGLE_CREDENTIALS),
-        scope
-    )
-)
-
-spreadsheet = client.open_by_key(SHEET_ID)
-
-sheet = spreadsheet.worksheet("MENU")
-config_sheet = spreadsheet.worksheet("CONFIG")
 
 # ==========================
 # COLUNAS SEGURAS
