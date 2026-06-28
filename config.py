77854f259426507b4ae694200afe759a4c178792
Jfelpz <joaozimalencar@gmail.com>
2026-06-28 20:25:55 -0300

    config.py

--- config.py
+++ config.py
@@ -1,20 +1,13 @@
 import os
 import json
-import uuid
-import time
 import gspread
-import requests
-from datetime import datetime
 from oauth2client.service_account import ServiceAccountCredentials
-from zoneinfo import ZoneInfo
 
 # ==========================
 # AUTHENTICADOR
 # ==========================
 
 SHEET_ID = os.getenv("SHEET_ID")
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
 GOOGLE_CREDENTIALS = os.getenv("GOOGLE_CREDENTIALS")
 
 scope = [
