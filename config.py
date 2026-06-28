e67aaf04e338216cb0f0c8fbdee982c4f0ba1fc0
Jfelpz <joaozimalencar@gmail.com>
2026-06-28 20:46:42 -0300

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
 import os
-import json
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-
 # ==========================
 # AUTHENTICADOR
 # ==========================
@@ -10,20 +6,3 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 SHEET_ID = os.getenv("SHEET_ID")
 GOOGLE_CREDENTIALS = os.getenv("GOOGLE_CREDENTIALS")
-
-scope = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/drive"
-]
-
-client = gspread.authorize(
-    ServiceAccountCredentials.from_json_keyfile_dict(
-        json.loads(GOOGLE_CREDENTIALS),
-        scope
-    )
-)
-
-spreadsheet = client.open_by_key(SHEET_ID)
-
-sheet = spreadsheet.worksheet("MENU")
-config_sheet = spreadsheet.worksheet("CONFIG")
