cd07bf60d05d6d14a91764d354c078aa0ad49887
Jfelpz <joaozimalencar@gmail.com>
2026-06-28 23:04:14 -0300

    logic.py

--- logic.py
+++ logic.py
@@ -1,11 +1,9 @@
 import uuid
-import time
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
 from sheets import sheet
-from config import INTERVALO_MINUTOS, DESCONTO_MINIMO, LIMITE_POSTS
-from sheets import config_sheet
+from config import DESCONTO_MINIMO, LIMITE_POSTS
 
 
 def processar(enviar):
@@ -24,37 +22,37 @@ def processar(enviar):
 
         if enviados >= LIMITE_POSTS:
             break
-    
+
         try:
             status = str(row[col("STATUS")] or "").strip().upper()
-    
+
             if status == "ENVIADO":
                 continue
-    
+
             produto = row[col("PRODUTO")]
             preco = row[col("PREÇO")]
             link = row[col("LINK_AFILIADO")]
             desconto = row[col("DESCONTO")]
-    
+
         except Exception as e:
             print("❌ ERRO LINHA", row_number, e)
             continue
-    
-        # 👇 AQUI (MESMO NÍVEL DOS OUTROS BLOCOS)
+
+        # ID
         id_col = col("ID")
-    
+
         if not row[id_col]:
             produto_id = str(uuid.uuid4())[:8]
             sheet.update_cell(row_number, id_col + 1, produto_id)
             print("🆔 ID GERADO:", produto_id)
-    
+
         try:
             desconto_valor = float(
                 str(desconto).replace("%", "").replace(",", ".")
             )
         except:
             desconto_valor = 0
-    
+
         if desconto_valor < DESCONTO_MINIMO:
             continue
 
