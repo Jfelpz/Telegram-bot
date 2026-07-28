5eedf24405b0a06a08361faf90cc689896070148
Jfelpz <joaozimalencar@gmail.com>
2026-07-27 22:52:51 -0300

    Create teste_magalu.py

---
+++ coletores/teste_magalu.py
@@ -0,0 +1,8 @@
+from coletores.magalu import coletar_magalu
+
+url = "https://www.magazinevoce.com.br/magazinegrupaodapromocao/smartphone-samsung-a07-128gb-preto-4gb-ram-tela-67-cam-dupla-selfie-8mp/p/240466500/te/ga07/"
+
+dados = coletar_magalu(url)
+
+for chave, valor in dados.items():
+    print(f"{chave}: {valor}")
