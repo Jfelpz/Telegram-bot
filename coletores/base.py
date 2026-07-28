f7be4ab36a3816c08eda65d64e9be35a5d17af71
Jfelpz <joaozimalencar@gmail.com>
2026-07-27 22:48:26 -0300

    Update base.py

--- coletores/base.py
+++ coletores/base.py
@@ -1,39 +1,44 @@
 from playwright.sync_api import sync_playwright
+import json
 
 URL = "https://www.magazinevoce.com.br/magazinegrupaodapromocao/smartphone-samsung-a07-128gb-preto-4gb-ram-tela-67-cam-dupla-selfie-8mp/p/240466500/te/ga07/"
 
 with sync_playwright() as p:
 
     browser = p.chromium.launch(headless=True)
-
     page = browser.new_page()
 
     page.goto(URL, wait_until="networkidle")
 
-    scripts = page.locator("script")
-
-    print(f"Total de scripts: {scripts.count()}")
-
-    for i in range(scripts.count()):
-
-        script = scripts.nth(i)
-
-        try:
-            script_id = script.get_attribute("id")
-            script_type = script.get_attribute("type")
-
-            if script_id or script_type == "application/json":
-
-                print("=" * 80)
-                print(f"SCRIPT {i}")
-                print("id:", script_id)
-                print("type:", script_type)
-
-                texto = script.inner_text()
-
-                print(texto[:5000])
-
-        except Exception:
-            pass
+    dados = page.locator("#__NEXT_DATA__").inner_text()
 
     browser.close()
+
+    print("Tamanho do JSON:", len(dados))
+
+    palavras = [
+        "240466500",
+        "bestPrice",
+        "listPrice",
+        "offers",
+        "price",
+        "Samsung A07",
+        "available"
+    ]
+
+    print("\n")
+
+    for palavra in palavras:
+        print(f"{palavra}: {palavra in dados}")
+
+    print("\n")
+
+    indice = dados.find("240466500")
+
+    print("Indice:", indice)
+
+    if indice != -1:
+        inicio = max(0, indice - 2000)
+        fim = indice + 6000
+
+        print(dados[inicio:fim])
