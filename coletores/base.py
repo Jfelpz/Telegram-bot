a3f4c835981032e40de61acceb8fdf9ca8f55181
Jfelpz <joaozimalencar@gmail.com>
2026-07-27 23:23:47 -0300

    Update base.py

--- coletores/base.py
+++ coletores/base.py
@@ -1,9 +1,40 @@
-from coletores.magalu import coletar_magalu
+from urllib.parse import urlparse
+
+from coletores.magalu import MagaluCollector
 
 
-def coletar(url: str):
+class ColetorBase:
 
-    if "magazinevoce" in url or "magazineluiza" in url:
-        return coletar_magalu(url)
+    def __init__(self):
 
-    raise ValueError("Loja não suportada.")
+        self.coletores = {
+            "magazinevoce.com.br": MagaluCollector(),
+            "magazineluiza.com.br": MagaluCollector(),
+        }
+
+    def identificar_loja(self, url: str):
+
+        dominio = urlparse(url).netloc.lower()
+
+        dominio = dominio.replace("www.", "")
+
+        for site, coletor in self.coletores.items():
+
+            if site in dominio:
+                return coletor
+
+        raise Exception(f"Loja não suportada: {dominio}")
+
+    def coletar(self, url: str):
+
+        coletor = self.identificar_loja(url)
+
+        return coletor.coletar(url)
+
+
+coletor = ColetorBase()
+
+
+def coletar_produto(url):
+
+    return coletor.coletar(url)
