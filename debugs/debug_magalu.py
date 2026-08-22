c0637ad4aa39fd1251587c8a62d2086bbdfbc9a6
Jfelpz <joaozimalencar@gmail.com>
2026-08-21 23:12:17 -0300

    Update debug_magalu.py

--- debugs/debug_magalu.py
+++ debugs/debug_magalu.py
@@ -1,60 +1,26 @@
-import json
-from pprint import pprint
-
 from coletores.magalu import MagaluCollector
 
-URL = "https://www.magazinevoce.com.br/magazinegrupaodapromocao/smartphone-samsung-a07-256gb-preto-8gb-ram-tela-67-cam-dupla-selfie-8mp/p/240466000/te/ga07/"
 
-collector = MagaluCollector()
+print("=" * 50)
+print("TESTANDO COLETOR MAGAZINE LUIZA")
+print("=" * 50)
 
-html = collector._baixar_html(URL)
+coletor = MagaluCollector()
 
-dados = collector._extrair_json(html)
+url = (
+    "https://www.magazinevoce.com.br/"
+    "magazinegrupaodapromocao/"
+    "smartphone-samsung-a07-256gb-preto-8gb-ram-"
+    "tela-67-cam-dupla-selfie-8mp/"
+    "p/240466000/te/ga07/"
+)
 
-produto = collector._produto(dados)
-
-print("=" * 80)
-print("CHAVES DO PRODUTO")
-print("=" * 80)
-
-print(produto.keys())
-
-print()
-
-print("=" * 80)
-print("OBJETO PRICE")
-print("=" * 80)
-
-pprint(produto.get("price"))
+resultado = coletor.coletar(url)
 
 print()
+print("=" * 50)
+print("RESULTADO")
+print("=" * 50)
 
-print("=" * 80)
-print("OBJETO SELLER")
-print("=" * 80)
-
-pprint(produto.get("seller"))
-
-print()
-
-print("=" * 80)
-print("OBJETO OFFERS")
-print("=" * 80)
-
-pprint(produto.get("offers"))
-
-print()
-
-print("=" * 80)
-print("OBJETO INSTALLMENT")
-print("=" * 80)
-
-pprint(produto.get("installment"))
-
-print()
-
-print("=" * 80)
-print("RESULTADO DO COLETOR")
-print("=" * 80)
-
-pprint(collector.coletar(URL))
+for chave, valor in resultado.items():
+    print(f"{chave}: {valor}")
